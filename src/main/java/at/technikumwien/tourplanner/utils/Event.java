923f4b0098fa6ae4a82e9d0de7f5d9ee217eedc1
Noah Howadt <noah.howadt@gmail.com>
2025-05-18 18:41:20 +0200

    Add input validation for distance field in EditTourController and ViewModel

--- src/main/java/at/technikumwien/tourplanner/utils/Event.java
+++ src/main/java/at/technikumwien/tourplanner/utils/Event.java
@@ -4,5 +4,5 @@ public class Event {
     public static final String TOUR_SELECTED = "tourSelected";
     public static final String CANCEL_EDIT = "cancelEdit";
     public static final String EDIT_TOUR = "editTour";
-    // Add more event constants as needed
+    public static final String VALIDATION_ERROR = "validationError";
 }
