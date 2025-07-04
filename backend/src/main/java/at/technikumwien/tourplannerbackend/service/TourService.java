59598ea9d3cb77984b8ea05f7b640ebc72809437
Marvin <m.loehlein@gmx.at>
2025-07-04 23:41:56 +0200

    implemented logging for Tourservice

--- backend/src/main/java/at/technikumwien/tourplannerbackend/service/TourService.java
+++ backend/src/main/java/at/technikumwien/tourplannerbackend/service/TourService.java
@@ -4,11 +4,14 @@ import at.technikumwien.tourplannerbackend.model.TourItem;
 import at.technikumwien.tourplannerbackend.repository.TourItemRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class TourService {
     private final TourItemRepository repository;
     private final OpenRouteService openRouteService;
+    private static final Logger logger = LoggerFactory.getLogger(TourService.class);
 
     public TourService(TourItemRepository repository, OpenRouteService openRouteService) {
         this.repository = repository;
@@ -16,27 +19,34 @@ public class TourService {
     }
 
     public List<TourItem> getAllTours() {
+        logger.info("Get all tours");
         return repository.findAll();
     }
 
     public TourItem saveTour(TourItem tour) {
+        logger.info("Saving new tour: {}", tour);
         double distance = this.openRouteService.calculateDistance(tour.getStartLocation(), tour.getEndLocation());
         tour.setDistance(distance);
 
         double duration = this.openRouteService.calculateDuration(tour.getStartLocation(), tour.getEndLocation());
         tour.setEstimatedTime(duration);
 
-        System.out.println("tour estimated time is " + tour.getEstimatedTime());
-        System.out.println("tour duration is " + tour.getDistance());
+        logger.debug("Calculated distance: {}", distance);
+        logger.debug("Estimated duration: {}", duration);
 
-        return repository.save(tour);
+        TourItem saved = repository.save(tour);
+        logger.info("Tour saved successfully with ID: {}", saved.getId());
+
+        return saved;
     }
 
     public void deleteTour(String id) {
+        logger.info("Deleting tour with ID: {}", id);
         repository.deleteById(id);
     }
 
     public TourItem getTourById(String id) {
+        logger.info("Get tour with ID: {}", id);
         return repository.findById(id).orElse(null);
     }
 }
